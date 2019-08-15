87d8c2a83c10fcfb6dc368e9e274e09a2683a7f3
Jarred Reid <51389325+JREID96@users.noreply.github.com>
2019-08-15 12:07:19 -0400

    Update script.ps1
    Intranet issue fixed

--- script.ps1
+++ script.ps1
@@ -1,9 +1,17 @@
-### KioskCreator v1.2.0
+### KioskCreator v1.2.3
 ### This script automates the creation of kiosk computers
 ### Jarred Reid - 2019
 
 Clear-Host
 
+Write-Host "KioskCreator"
+Write-Host "This script automates the creation of kiosk computers."
+
+Write-Host
+
+#-#-#-#-#-#-#-#-#-#
+### Begin Stage 1
+#-#-#-#-#-#-#-#-#-#
 function InstallStageOne {
 
     ### Create kioskmode user (Here you are first creating the user, then piping to set the properties of said user.)
@@ -57,67 +65,70 @@ function InstallStageOne {
     Write-Host 'LibreOffice installed.'
 
     Write-Host
-
-    ### Log out of admin account
-    Write-Host "You will now be logged out of the Administrator account."
-    Write-Host "Please sign in as the 'Kiosk User' account and run this script again."
-    Read-Host -Prompt "Press any key to continue."
-
-    Write-Host 
-
+    
     Write-Host "Creating checkpoint file...."
     New-Item C:\Temp\MasterControl\stage1complete.mcka
     Write-Host "Checkpoint file has been created."
 
+    Write-Host
+
+    ### Log out of admin account
+    Write-Host "You will now be logged out of the Administrator account."
+    Write-Host "Please sign in as the 'Kiosk User' account and run this script again."
+    Read-Host -Prompt "Press the Enter key to continue."
+
     shutdown -l
 }
+#-#-#-#-#-#-#-#-#-#
+### End Stage 1
+#-#-#-#-#-#-#-#-#-#
 
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
+#-#-#-#-#-#-#-#-#-#
+### Begin Stage 2
+#-#-#-#-#-#-#-#-#-#
 function InstallStageTwo {
-### Configure MasterControl shortcuts
 
+### Configure MasterControl shortcuts
 Write-Host "Installing MasterControl shortcut...."
 Copy-Item -Path "C:\Temp\MasterControl\shortcut\MasterControl.lnk" -Destination "C:\Users\kioskmode\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 Remove-Item -Path "C:\Users\kioskmode\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Internet Explorer.lnk"
 Write-Host "Shortcut installed."
-### Set ACL permissions to allow Admin access to kioskmode user folder ########################################
 
 Write-Host
 
 ### Get the SID for the kioskmode user
-Write-Host "Obtaining SID...."
+Write-Host "Obtaining 'kioskmode' SID...."
 $User = New-Object System.Security.Principal.NTAccount("kioskmode")
 $sid = $User.Translate([System.Security.Principal.SecurityIdentifier]).value
 Write-Host "SID obtained."
 
 Write-Host
 
-### Inject SID into Registry Keys
-Write-Host "Injecting regkeys into registry...."
-(Get-Content "C:\Temp\MasterControl\regkeys\Set_Restrictions.reg") -replace 'insertSIDhere', $sid | Set-Content "C:\Temp\MasterControl\regkeys\Set_Restrictions.reg"
-regedit.exe /s "C:\Temp\MasterControl\regkeys\Set_Restrictions.reg"
-Write-Host "Regkeys injected."
-
-Write-Host
-
 ### Enable Dummy Proxy for LAN
-Write-Host "Configuring Internet Explorer proxy settings...."
 $regKey="HKU:\$sid\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 $proxy = "0.0.0.0:80"
 $whitelist = {}
+###
 
 Write-Host
 
 ### Mount HKEY_USERS as a PSDrive in order to manipulate the registry
-Write-Host "Mounting HKEY_USERS hive.... " 
+Write-Host "Mounting HKEY_USERS registry hive.... " 
 New-PSDrive HKU Registry HKEY_USERS 
-Write-Host "Hive mounted."
+Write-Host "Registry hive mounted."
 
+Write-Host "Configuring proxy "
 Set-ItemProperty -path $regkey ProxyEnable 1
 Set-ItemProperty -path $regKey ProxyServer -value $proxy
 Set-ItemProperty -path $regKey ProxyOverride -value $whitelist
-Write-Host "Proxy configured and enabled."
+Write-Host "Proxy configured."
+
+### Inject SID into Registry Keys
+Write-Host "Configuring Internet Explorer security settings...."
+(Get-Content "C:\Temp\MasterControl\regkeys\IE_Settings.reg") -replace 'insertSIDhere', $sid | Set-Content "C:\Temp\MasterControl\regkeys\IE_Settings.reg"
+Start-Process "regedit.exe" -Argument "/s C:\Temp\MasterControl\regkeys\IE_Settings.reg" -Verb RunAs
+Write-Host "Security settings have been applied."
 
 ### Install MasterControl Provisioned Package
 Write-Host "Installing provisioned package...."
@@ -128,24 +139,39 @@ Write-Host
 
 Write-Host "Removing checkpoint file...."
 Remove-Item C:\Temp\MasterControl\stage1complete.mcka
-Write-Host "File removed."
+Write-Host "Checkpoint file removed."
 
-Read-Host -prompt "MasterControl kiosk has been configured successfully. Press any key to reboot the machine."
-shutdown -r -t 30
+Read-Host -prompt "Kiosk has been configured successfully. Press the Enter key to reboot the machine."
+
+shutdown -r -t 0
 }
+#-#-#-#-#-#-#-#-#-#
+### End Stage 2
+#-#-#-#-#-#-#-#-#-#
 
 
 $file = "C:\Temp\MasterControl\stage1complete.mcka"
 if((Test-Path $file))
 {
     Write-Host "Previous install configuration detected. Please make sure you are signed in as the 'kioskmode' user inside a non-elevated Powershell window"
-    Read-Host -Prompt "Press any key to execute the script."
+    Read-Host -Prompt "Press the Enter key to execute the script."
     InstallStageTwo
 }
 
 if(!(Test-Path $file))
 {
-    Write-Host "No previous install configuration detected. Please run this script in an elevated Powershell window."
-    Read-Host -Prompt "Press any key to execute the script."
+    Write-Host "No previous install configuration detected."
+
+    # Elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    Read-Host -prompt "The Powershell window needs to be run as an administrator. Press the Enter key to elevate...."
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+     Exit
+    }
+   }
+   
+    Read-Host -Prompt "Press the Enter key to execute the script."
     InstallStageOne
 }
